d71e4c22736dccb05eec8ca02e6e978a9e63d4bf
LEH1CH <usman0val@vk.com>
2023-04-27 21:37:09 +0300

    fix: 1.8

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,14 +4,19 @@ export default class Section {
     this._container = document.querySelector(containerSelector);
   }
 
+  _clear() {
+    this._container.innerHTML = "";
+  }
+
   //Метод создания одиночной карточки и добавления её в DOM
   addItem(cardData, id = "") {
     const cardElement = this._renderer(cardData, id);
-    this._container.append(cardElement);
+    this._container.prepend(cardElement);
   }
 
   //Метод создания карточек исходного массива
   renderInitialCards(items, id) {
-    items.forEach((item) => this.addItem(item, id));
+    this._clear();
+    items.reverse().forEach((item) => this.addItem(item, id));
   }
 }
